6fc0eb1cc5ec26cbb36463cc340f9802f65a1d3d
Jawad63 <jawadqureshi111@gmail.com>
2022-07-04 19:22:27 +0200

    form complete (sass part)

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,11 +1,50 @@
 import React from "react";
+import logo from '../images/logo.png';
 
 const SignUpForm = () => {
     return (
         <div className="container">
-            <div className="wrapper">
-                <h2><span>vul uw </span>gegevens aan</h2>
+            <div className="img">
+                <img src={logo} />
             </div>
+            <div className="wrapper">
+                <p className="heading"><span>vul uw</span> gegevens aan</p>
+
+                <div className="form">
+                    <div className="input-box">
+                        <input type="name" placeholder="Voornaam" />
+                        <input type="name" placeholder="Naam" />
+                    </div>
+
+                    <input type="name" placeholder="Nummerplaat" />
+                    <br />
+                    <input type="name" placeholder="Email" />
+                    <br />
+                    <input type="name" placeholder="Straat" />
+
+                    <div className="input-box">
+                        <input type="name" placeholder="Nummer" />
+                        <input type="name" placeholder="Bus" />
+                    </div>
+
+                    <div className="input-box">
+                        <input type="name" placeholder="Stad" />
+                        <input type="name" placeholder="Postcode" />
+                    </div>
+
+                    <input className="input" type="name" placeholder="Telefoon" />
+                    <br />
+                    
+                    <div className="input-box">
+                        <input type="checkbox" />
+                        <p>Hou mij op de hoogte wanneer er interessante acties zijn bij Q Team.</p>
+                    </div>
+                </div>
+
+
+
+            </div>
+
         </div>
     )
 }
